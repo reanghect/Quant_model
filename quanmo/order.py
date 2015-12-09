f9559c5a1baa031dad210e319db50c5ef3626d9e
Will <reanghect@gmail.com>
2015-12-09 01:53:42 -0800

    new

--- quanmo/order.py
+++
@@ -1,8 +0,0 @@
-class Order(Account):
-	def __init__(self, symbol, direction, amount, tranact_price,state = Normal)
-		self.symbol = symbol
-		self.direction = direction
-		self.amount = amount
-		self.tranact_price = tranact_price
-		self.state = state
-	
