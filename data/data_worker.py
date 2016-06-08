b7eed56767277831bbfd049d862dbd920e39d04a
Will <reanghect@gmail.com>
2016-06-08 03:11:37 -0700

    logger in wrapper

--- data/data_worker.py
+++ data/data_worker.py
@@ -2,6 +2,7 @@ import tushare as ts
 from util import logger
 from datetime import datetime
 from data import database_model as db
+from pandas import Series
 
 _data_logger = logger.set_logger("module")
 
@@ -17,13 +18,16 @@ def get_new_data():
 def insert_new_data(new_data):
     time_stamp = datetime.now().time()
     for i in new_data.index:
-        db.IntraPrice.create(ticker=new_data.ix[i].code, time=time_stamp, high=new_data.ix[i].high, low=new_data.ix[i].low,
-                             open=new_data.ix[i].open, trade=new_data.ix[i].trade, change_percent=new_data.ix[i].changepercent)
+        db.IntraPrice.create(ticker=new_data.ix[i].code, time=time_stamp, high=new_data.ix[i].high,
+                             low=new_data.ix[i].low, open=new_data.ix[i].open, trade=new_data.ix[i].trade,
+                             change_percent=new_data.ix[i].changepercent)
+        _data_logger.debug("Insert " + str(time_stamp) + " hourly data into IntroPrice")
 
 
 def clean_intra():
     daily_delete = db.IntraPrice.delete()
     daily_delete.execute()
+    _data_logger.debug("Delete IntraPrice daily")
 
 
 def insert_daily_data():
@@ -36,12 +40,23 @@ def insert_daily_data():
                                  high=float(current.high[0]), low=float(current.low[0]), open=float(current.open[0]),
                                  close=float(current.close[0]), volume=float(current.volume[0]))
         except ConnectionError as e:
+            _data_logger.debug(today + "Can't fetch daily data due to Connection Error")
             print(e)
 
 
 def get_db_price(ticker=None, start=None, end=None, fields=None, table='daily'):
     if table == 'daily':
-        raw_data = db.DailyPrice.select(fields).where(db.DailyPrice.ticker == ticker,
-                                                      start < db.DailyPrice.trading_date < end)
-        return raw_data
-        # return in DataFrame type
+        raw_data = db.DailyPrice.select(fields)\
+            .where(db.DailyPrice.ticker == ticker, start < db.DailyPrice.trading_date < end)\
+            .order_by(db.DailyPrice.trading_date)
+    else:
+        raw_data = db.IntraPrice.select(fields)\
+            .where(db.IntraPrice.ticker == ticker, start < db.IntraPrice.time < end)\
+            .order_by(db.IntraPrice.time)
+
+    price = list()
+    index = list()
+    for record in raw_data:
+        price.append(record.fields[0])
+        index.append(record.trading_date[0])
+    return Series(price, index=index)
--- strategy/strat01.py
+++ strategy/strat01.py
@@ -1,4 +1,11 @@
+from data import data_worker as dw
+
 # Current Data INPUT
+
+hour_data = dw.get_new_data()
+
+dw.insert_new_data(hour_data)
+
 
 # Get Historical Data from DB
 
